66338c36c2009d0eaaf08610212b6e41f0c51db8
omaus <omaus@bio.uni-kl.de>
2022-09-15 18:25:34 +0200

    Add structure checks & tests
    Also moved types into checkArcStructure.fsx.

--- src/validateArc.fsx
+++ src/validateArc.fsx
@@ -6,43 +6,9 @@
 open CheckArcStructure
 
 open Expecto
+open Expecto.Expect
 
 open System.IO
-
-
-// Types:
-
-type StudyFolderStructure = {
-    Name                : string
-    Path                : string
-    HasIsaFile          : bool
-    HasResourcesFolder  : bool
-    HasProtocolsFolder  : bool
-}
-
-let createStudyFolderStructure name path hasIsaFile hasResourcesFolder hasProtocolsFolder = {
-    Name                = name
-    Path                = path
-    HasIsaFile          = hasIsaFile
-    HasResourcesFolder  = hasResourcesFolder
-    HasProtocolsFolder  = hasProtocolsFolder
-}
-
-type AssayFolderStructure = {
-    Name                : string
-    Path                : string
-    HasIsaFile          : bool
-    HasDatasetFolder    : bool
-    HasProtocolsFolder  : bool
-}
-
-let createAssayFolderStructure name path hasIsaFile hasDatasetFolder hasProtocolsFolder = {
-    Name                = name
-    Path                = path
-    HasIsaFile          = hasIsaFile
-    HasDatasetFolder    = hasDatasetFolder
-    HasProtocolsFolder  = hasProtocolsFolder
-}
 
 
 // Pre-stuff:
@@ -62,9 +28,9 @@ let hasArcFolder = checkForArcFolder pathToCheck
 
 let hasGitFolderStructure = checkForGitFolderStructure pathToCheck
 
-let hasStudiesFolder = checkForAssayFolder pathToCheck
+let hasStudiesFolder = checkForStudiesFolder pathToCheck
 
-let hasAssayFolder = checkForAssayFolder pathToCheck
+let hasAssaysFolder = checkForAssaysFolder pathToCheck
 
 let hasRunsFolder = checkForRunsFolder pathToCheck
 
@@ -77,8 +43,23 @@ let studiesInStudiesFolder =
     else None
 
 let assaysInAssaysFolder =
-    if hasAssayFolder then Directory.GetDirectories assaysFolder |> Some
+    if hasAssaysFolder then Directory.GetDirectories assaysFolder |> Some
     else None
+
+let studiesFolderStructure =
+    match studiesInStudiesFolder with
+    | None -> None
+    | Some studies ->
+        studies
+        |> Array.map (
+            fun dir ->
+                let n = (DirectoryInfo dir).Name
+                let isaf = Path.Combine(dir, "isa.assay.xlsx") |> File.Exists
+                let rf = Path.Combine(dir, "resources") |> Directory.Exists
+                let pf = Path.Combine(dir, "protocols") |> Directory.Exists
+                createStudyFolderStructure n dir isaf rf pf
+        )
+        |> Some
 
 let assaysFolderStructure =
     match assaysInAssaysFolder with
@@ -87,10 +68,57 @@ let assaysFolderStructure =
         assays
         |> Array.map (
             fun dir ->
-                let p = Path.Combine(dir, "isa.xlsx")
                 let n = (DirectoryInfo dir).Name
-                let 
+                let isaf = Path.Combine(dir, "isa.assay.xlsx") |> File.Exists
+                let df = Path.Combine(dir, "dataset") |> Directory.Exists
+                let pf = Path.Combine(dir, "protocols") |> Directory.Exists
+                createAssayFolderStructure n dir isaf df pf
         )
+        |> Some
 
 
 // Validation (Tests):
+
+let fileStructureTests = testList "File structure tests" [
+    testCase "Has .arc folder" <| fun () -> isTrue hasArcFolder ".arc folder does exist"
+    testCase "Has Git folder structure" <| fun () -> isTrue hasGitFolderStructure "Git folder structure does exist"
+    testCase "Has Studies folder" <| fun () -> isTrue hasStudiesFolder "Studies folder does exist"
+    testCase "Has Assays folder" <| fun () -> isTrue hasAssaysFolder "Assays folder does exist"
+    testCase "Has Runs folder" <| fun () -> isTrue hasRunsFolder "Runs folder does exist"
+    testCase "Has Workflows folder" <| fun () -> isTrue hasWorkflowsFolder "Workflows folder does exist"
+    testCase "Has ISA Investigation file" <| fun () -> isTrue hasInvestigationFile "ISA Investigation file does exist"
+    match studiesFolderStructure with
+    | None -> ()
+    | Some studies ->
+        studies
+        |> Array.map (
+            fun s ->
+                testList $"Tests for Study {s.Name}" [
+                    testCase $"Study {s.Name} has ISA study file" <| fun () -> isTrue s.HasIsaFile "ISA Study file does exist"
+                    testCase $"Study {s.Name} has Resources folder" <| fun () -> isTrue s.HasResourcesFolder "Resources folder does exist"
+                    testCase $"Study {s.Name} has Protocols folder" <| fun () -> isTrue s.HasProtocolsFolder "Protocols folder does exist"
+                ]
+        )
+        |> Array.toList
+        |> testList "Individual Studies tests"
+    match assaysFolderStructure with
+    | None -> ()
+    | Some assays ->
+        assays
+        |> Array.map (
+            fun a ->
+                testList $"Tests for Assay {a.Name}" [
+                    testCase $"Assay {a.Name} has ISA assay file" <| fun () -> isTrue a.HasIsaFile "ISA Assay file does exist"
+                    testCase $"Assay {a.Name} has Dataset folder" <| fun () -> isTrue a.HasDatasetFolder "Dataset folder does exist"
+                    testCase $"Assay {a.Name} has Protocols folder" <| fun () -> isTrue a.HasProtocolsFolder "Protocols folder does exist"
+                ]
+        )
+        |> Array.toList
+        |> testList "Individual Assays tests"
+]
+
+let isaStandardTests = testList "ISA standard tests" [
+    
+]
+
+let testsCombined = testList "Tests combined" [fileStructureTests]
