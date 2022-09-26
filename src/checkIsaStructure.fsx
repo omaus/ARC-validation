a57e4c45585714e5d3d6b926927a0ba44d5de2bf
omaus <omaus@bio.uni-kl.de>
2022-09-26 18:02:45 +0200

    Add intersection functions to collection modules

--- src/checkIsaStructure.fsx
+++ src/checkIsaStructure.fsx
@@ -6,6 +6,38 @@ open FsSpreadsheet
 open ISADotNet
 open ISADotNet.XLSX
 
+module Seq =
+    /// Computes the intersection of two sequences.
+    let intersect (seq1 : seq<'T>) seq2 = seq {
+        let smallerSeq, largerSeq =
+            if Seq.length seq1 >= Seq.length seq2 then seq2, seq1
+            else seq1, seq2
+        for e in smallerSeq do
+            if Seq.contains e largerSeq then e
+    }
+
+module Array =
+    /// Computes the intersection of two arrays.
+    let intersect (arr1 : 'T []) (arr2 : 'T []) =
+        let smallerArr, largerArr =
+            if arr1.Length >= arr2.Length then arr2, arr1
+            else arr1, arr2
+        smallerArr
+        |> Array.filter (fun e -> Array.contains e largerArr)
+
+module List =
+    /// Computes the intersection of two lists.
+    let intersect (list1 : 'T list) (list2 : 'T list) =
+        let smallerList, largerList =
+            if list1.Length >= list2.Length then list2, list1
+            else list1, list2
+        let rec loop predicate l1 l2 fl =
+            match l1 with
+            | h :: t -> if predicate h l2 then loop predicate t l2 (h :: fl) else loop predicate t l2 fl
+            | [] -> fl
+        loop List.contains smallerList largerList []
+
+// TO DO: Add outersection functions to modules!
 
 /// Checks if all existing Studies are registered in the Investigation file and if all registered Studies in the Investigation file are present in the ARC.
 let areStudiesRegistered studiesPaths invesPath =
@@ -19,7 +51,7 @@ let areStudiesRegistered studiesPaths invesPath =
             |> Array.map StudyFile.Study.fromFile
     let setSfis =
         match studiesFromInves with
-        | Some sfis -> set sfis     // TO DO: URGENT! Add comparison to types Study, Assay and (maybe) Investigation to support sets!
+        | Some sfis -> set sfis
         | None -> set []
     let setSffs = set studiesFromFiles
     setSffs
