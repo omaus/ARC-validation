0d8dff9248be7fb843b76f57d1163d0483b403db
omaus <omaus@bio.uni-kl.de>
2022-12-05 16:23:56 +0100

    Add another test project

---
+++ src/DocuTestProject/Tests.fs
@@ -0,0 +1,14 @@
+﻿module Tests
+
+open System
+open NUnit.Framework
+
+[<TestFixture>]
+type TestClass () =
+
+    [<Test>]
+    member this.TestMethodPassing() =
+        Assert.True(true)
+
+    [<Test>]
+     member this.FailEveryTime() = Assert.True(false)
