61e9f579dc8ffbca6cbae6922428b7e3115feb6a
omaus <omaus@bio.uni-kl.de>
2023-02-09 18:03:34 +0100

    Add function body to check result

--- src/ArcValidationWebApp/ValidationResultApi.fs
+++ src/ArcValidationWebApp/ValidationResultApi.fs
@@ -1,11 +1,36 @@
 ﻿module ValidationResultApi
 
+open Giraffe
+open System.IO
+
 // KF: header im Post-Request-JSON hat URL vom Absender und kann somit benutzt werden, um 
 
 module Handler =
 
     let checkResult : HttpHandler =
         fun (next : HttpFunc) (ctx : HttpContext) -> 
+            // modified from: https://stackoverflow.com/questions/17232414/creating-a-zip-archive-in-memory-using-system-io-compression
+            task {
+                let testResult = ctx.Request.Body
+                let ms = new MemoryStream()
+                let! _ = testResult.CopyToAsync(ms)
+                let testResultBA = ms.ToArray()
+
+                let getByteArray (fileName : string) (data : byte []) =
+                    try
+                        use ms = new MemoryStream()
+                        (
+                            use archive = new ZipArchive(ms, ZipArchiveMode.Create)
+                            let entry = archive.CreateEntry(fileName)
+                            use entryStream = entry.Open()
+                            use bw = new BinaryWriter(entryStream)
+                            bw.Write(data)
+                        )
+                        (ms.ToArray())
+                    with e -> failwithf "Cannot zip stream %s: %s" fileName e.Message
+                let res = getByteArray "arc.json" isaJsonBA
+                return! ctx.WriteBytesAsync res
+            }
 
 module Docs =
 
